e0f93b53f9df841b6c86bdcb85ab8c5cc17e9051
Georgi <ggeorgeuk@gmail.com>
2025-06-15 12:05:42 +0100

    ForgotPassword component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import PrivateRoute from "./layouts/PrivateRoute";
 import Register from "../src/views/auth/Register";
 import Login from "../src/views/auth/Login";
 import Logout from "../src/views/auth/Logout";
+import ForgotPassword from "../src/views/auth/ForgotPassword";
+
 
 function App() {
 
@@ -13,9 +15,10 @@ function App() {
     <BrowserRouter>
       <MainWrapper>
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
+          <Route path="/register/" element={<Register />} />
+          <Route path="/login/" element={<Login />} />
+          <Route path="/logout/" element={<Logout />} />
+          <Route path="/forgot-password/" element={<ForgotPassword />} />
         </Routes>
       </MainWrapper>
     </BrowserRouter>
